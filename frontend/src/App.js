6bc0c25de0e299885bce49c42d98ac83922f228b
kevin jia <kevinjia1984@gmail.com>
2022-01-08 08:02:12 +1100

    use useState hook to bind word state to the type fields

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header';
 import Search from './components/search';
+import {useState} from 'react';
 
 const App = () => {
+    const [word, setWord] = useState('')
+
+    const handleSearchSubmit = (e) => {
+        e.preventDefault();
+        console.log(word)
+    }
+
+    // Testing for showing it is a controlled component
+    // everytime we change the state of the Search text the App component will be refreshed
+    console.log(word)
+
     return (
         <div>
             <Header title="Images Gallery"/>
-            <Search/>
+            <Search word={word} setWord={setWord} handleSubmit = {handleSearchSubmit}/>
         </div>
     );
 }
--- frontend/src/components/search.js
+++ frontend/src/components/search.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import {Button, Col, Container, Form, Row} from "react-bootstrap";
 
-const Search = () => {
+const Search = ({handleSubmit, word, setWord}) => {
     return (
         <Container className="mt-4">
             <Row className="justify-content-center">
                 <Col xs={12} md={8}>
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                         <Form.Row>
                             <Col xs={9}>
-                                <Form.Control type="email" placeholder="Search for new image..."/>
+                                <Form.Control
+                                    type="text"
+                                    value={word}
+                                    onChange={(e) => setWord(e.target.value)}
+                                    placeholder="Search for new image..."
+                                />
                             </Col>
                             <Col>
                                 <Button variant="primary" type="submit">Search</Button>
