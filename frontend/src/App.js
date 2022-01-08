e692309a3b38aac32c8891d01ad55c7bdd878257
kevin jia <kevinjia1984@gmail.com>
2022-01-08 16:43:06 +1100

    add images in state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,10 @@ import {useState} from 'react';
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
-    const [word, setWord] = useState('')
+    const [word, setWord] = useState('');
+    const [images, setImages] = useState([]);
+
+    console.log(images)
 
     const handleSearchSubmit = (e) => {
         e.preventDefault();
@@ -14,7 +17,8 @@ const App = () => {
         fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
             .then(res => res.json())
             .then(data => {
-                console.log(data)
+                // insert the new image in the beginning images list
+                setImages([data, ...images])
             })
             .catch(err => {
                 console.log(err);
