f86a9efc14625f4004229a332f38e0c79bbfc73d
kevin jia <kevinjia1984@gmail.com>
2022-01-12 22:17:30 +1100

    get saved images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header';
@@ -13,6 +13,20 @@ const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5050'
 const App = () => {
     const [word, setWord] = useState('');
     const [images, setImages] = useState([]);
+
+    const getSavedImages = async() => {
+        try {
+            const res = await axios.get(`${API_URL}/images`)
+            // TODO currently the res.data not including title
+            setImages(res.data || []);
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    // we should run useEffect only once when it loaded
+    // depend on nonthing will achieve that
+    useEffect(() => getSavedImages(), [])
 
     const deleteImage = (delete_id) => {
         setImages(images.filter((image) => {
--- frontend/src/components/imageCard.js
+++ frontend/src/components/imageCard.js
@@ -6,7 +6,9 @@ const ImageCard = ({image, deleteImage}) => {
         <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src={image.urls.small} />
             <Card.Body>
-                <Card.Title>{image.title.toUpperCase()}</Card.Title>
+                {/*<Card.Title>{image.title && image.title.toUpperCase()}</Card.Title>*/}
+                {/*new optional chaining syntax*/}
+                <Card.Title>{image.title?.toUpperCase()}</Card.Title>
                 <Card.Text>
                     {image.description || image.alt_description}
                 </Card.Text>
